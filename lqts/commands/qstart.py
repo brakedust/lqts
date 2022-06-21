ba4e20d7fbe37cc14168c438bc32082515d335f4
Bradley L Campbell <bradley.campbell@navy.mil>
2022-06-21 14:13:46 -0400

    Removed option to set ip address from qstart.  It wasn't used anyway.  Port number is exported as an environment variable.

--- lqts/commands/qstart.py
+++ lqts/commands/qstart.py
@@ -1,8 +1,8 @@
-from pathlib import Path
+import os
 import subprocess
+from pathlib import Path
 
 import click
-
 import lqts.environment
 from lqts.config import Configuration
 
@@ -16,14 +16,15 @@ else:
 @click.option("--no-popout", is_flag=True, default=False, help="By default the server is started in a new cmd prompt using "
     "windows's 'start' mechanism. This flag causes the server to start in this terminal instance and block.")
 @click.option("--port", default=config.port, help="The port number of the server")
-@click.option("--ip_address", default=config.ip_address, help="The IP address of the server")
-def qstart(no_popout=False, port=config.port, ip_address = config.ip_address):
+# @click.option("--ip_address", default=config.ip_address, help="The IP address of the server")
+def qstart(no_popout=False, port=config.port):  # , ip_address=config.ip_address):
     """Starts the LQTS queue server"""
     if no_popout:
         args = ["uvicorn", "--port", str(port), "--log-level", "warning", "lqts.server:app"]
     else:
         args = ["start", "uvicorn", "--port", str(port), "--log-level", "warning", "lqts.server:app"]
     print(" ".join(args))
+    os.environ['LQTS_PORT'] = str(port)
     subprocess.call(" ".join(args), shell=True)
 
 
